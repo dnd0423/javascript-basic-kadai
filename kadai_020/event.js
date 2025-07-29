7029023df523d14829e05679126ed37ba08de523
SimSeUng <dnd0423@gmail.com>
2025-07-29 14:19:59 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+
+const textbar = document.getElementById("text");
+
+
+btn.addEventListener("click" , () =>{
+    textbar.textContent = "ボタンをクリックしました";
+});
